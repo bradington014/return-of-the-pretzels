f15e8df117d16abd0401d9af13542e85ad3efaef
Justin-Koida-SA <100797123+Justin-Koida-SA@users.noreply.github.com>
2022-05-10 15:05:58 -0700

    added the sprites in scene

--- sprite.js
+++ sprite.js
@@ -1,15 +1,34 @@
 loadSprite("father", )
 loadSprite("wall", )
-loadSprite("Lars", )
-loadSprite("Tex", )
-loadSprite("Shelldon", )
-loadSprite("Reinette", )
-loadSprite("Raina", )
-loadSprite("Ivy", )
-loadSprite("Dwayne", )
-loadSprite("Shuihaizi", )
-loadSprite("Bartholomew", )
-loadSprite("pretzel",)
+loadSprite("Lars", "sprites/Lars.png")
+loadSprite("Tex", "sprites/Tex.png")
+loadSprite("Shelldon", "sprites/Shelldon.png")
+loadSprite("Rainette", "sprites/Rainette.png")
+loadSprite("Raina", "sprites/Raina.png")
+loadSprite("Ivy", "sprites/Ivy.png")
+loadSprite("Dwayne", "sprites/Dwayne.png")
+loadSprite("Shuihaizi", "sprites/Shuihaizi.png")
+loadSprite("Bartholomew", "sprites/Bartholomew.png")
 loadSprite("background", "sprites/sapbackground.png");
+
+loadSprite("pretzel", "/sprites/pretzelstrip.png", {
+    sliceX: 9,
+
+    anims: {
+        "idle": {
+            from: 0,
+            to: 0,
+            loop: true,
+        },
+        "run": {
+            from: 1,
+            to: 8,
+            loop: true,
+            speed: 10,
+        }
+    }
+}
+
+)
 
 
