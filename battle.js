296ebfb7426745943b1712c97bd9f1469c4b7130
Justin-Koida-SA <100797123+Justin-Koida-SA@users.noreply.github.com>
2022-05-11 21:16:07 -0700

    temp fix

--- battle.js
+++ battle.js
@@ -357,7 +357,6 @@ function larsPowers(){
         rect(width()/2,height()/2),
         pos(500,500),
         color(149,206,214,50),
-        origin("center")
     ])
 }
 
@@ -373,7 +372,7 @@ function shelldonPowers(){
     add([
         rect(width()/2,height()/2),
         pos(500,500),
-        color(149,206,214,50),
+        color(255,223,81,50),
     ])
 }
 
@@ -381,7 +380,7 @@ function rainettePowers(){
     add([
         rect(width()/2,height()/2),
         pos(500,500),
-        color(300,200,100,50),
+        color(173,15,0,50),
     ])
 }
 
@@ -389,7 +388,7 @@ function rainaPowers(){
     add([
         rect(width()/2,height()/2),
         pos(500,500),
-        color(200,300,100,50),
+        color(122,0,148,50),
     ])
 }
 
@@ -397,7 +396,7 @@ function ivyPowers(){
     add([
         rect(width()/2,height()/2),
         pos(500,500),
-        color(100,200,300,50),
+        color(0,202,20,50),
     ])
 }
 
@@ -424,4 +423,5 @@ function bartholomewPowers(){
         color(250,250,50,50),
     ])
 }
+
 
--- powers.js
+++ powers.js
@@ -17,28 +17,56 @@ function texPowers(){
 
 function shelldonPowers(){
     add([
-        rect(width/2,height/2,500,500),
+        rect(width()/2,height()/2),
+        pos(500,500),
         color(255,223,81,50),
     ])
 }
 
 function rainettePowers(){
     add([
-        rect(width/2,height/2,500,500),
+        rect(width()/2,height()/2),
+        pos(500,500),
         color(173,15,0,50),
     ])
 }
 
 function rainaPowers(){
     add([
-        rect(width/2,height/2,500,500),
+        rect(width()/2,height()/2),
+        pos(500,500),
         color(122,0,148,50),
     ])
 }
 
-function lvyPowers(){
+function ivyPowers(){
     add([
-        rect(width/2,height/2,500,500),
+        rect(width()/2,height()/2),
+        pos(500,500),
         color(0,202,20,50),
+    ])
+}
+
+function dwaynePowers(){
+    add([
+        rect(width()/2,height()/2),
+        pos(500,500),
+        color(300,300,200,50),
+    ])
+}
+
+function shuihaiziPowers(){
+    add([
+        rect(width()/2,height()/2),
+        pos(500,500),
+        color(250,300,150,50),
+    ])
+}
+
+function bartholomewPowers(){
+    add([
+        rect(width()/2,height()/2),
+        pos(500,500),
+        color(250,250,50,50),
     ])
 }
