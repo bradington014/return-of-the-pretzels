32b482460149f2b6207c7c340285eb3e0d79ae44
Justin-Koida-SA <100797123+Justin-Koida-SA@users.noreply.github.com>
2022-05-13 19:25:58 -0700

    health and lars/shelldon power work

--- powers.js
+++ powers.js
@@ -1,11 +1,20 @@
 import "./battle.js";
 
+let PTIME_COUNT = 0
+let freeze = "false"
+
 let larsPowers = function larsPowers(){
     add([
-        rect(width()/2,height()/2),
-        pos(width()/2,height()/2),
+        rect(width()/1.225,height()),
+        pos(width()/2.475,height()/2),
         color(149,206,214,50),
-        'larspowerup'
+        area(),
+        opacity(.5),
+        origin("center"),
+        area(),
+        lifespan(.1),
+        layer("wall"),
+        'larspowerup',
     ])
     
 }
@@ -15,16 +24,23 @@ let texPowers = function texPowers(){
     rect(width()/2,height()/2),
     pos(width()/2,height()/2),
     color(255,57,0,50),
+    area(),
 ])
 }
 
 let shelldonPowers = function shelldonPowers(){
     add([
-        rect(width()/2,height()/2),
-        pos(width()/2,height()/2),
-        color(255,223,81,50),
-        'shelldonpowerup'
+        rect(width()/1.225,height()),
+        pos(width()/2.475,height()/2),
+        color(255,223,81),
+        opacity(.5),
+        origin("center"),
+        area(),
+        lifespan(1),
+        layer("wall"),
+        'shelldonpowerup',
     ])
+  
 }
 
 let rainettePowers = function rainettePowers(){
@@ -32,6 +48,7 @@ let rainettePowers = function rainettePowers(){
         rect(width()/2,height()/2),
         pos(width()/2,height()/2),
         color(173,15,0,50),
+        area(),
     ])
 }
 
@@ -40,6 +57,7 @@ let rainaPowers = function rainaPowers(){
         rect(width()/2,height()/2),
         pos(width()/2,height()/2),
         color(122,0,148,50),
+        area(),
     ])
 }
 
@@ -48,6 +66,7 @@ let ivyPowers = function ivyPowers(){
         rect(width()/2,height()/2),
         pos(width()/2,height()/2),
         color(0,202,20,50),
+        area(),
     ])
 }
 
@@ -56,6 +75,7 @@ let dwaynePowers = function dwaynePowers(){
         rect(width()/2,height()/2),
         pos(width()/2,height()/2),
         color(300,300,200,50),
+        area(),
     ])
 }
 
@@ -64,6 +84,7 @@ let shuihaiziPowers = function shuihaiziPowers(){
         rect(width()/2,height()/2),
         pos(width()/2,height()/2),
         color(250,300,150,50),
+        area(),
     ])
 }
 
@@ -72,15 +93,44 @@ let bartholomewPowers = function bartholomewPowers(){
         rect(width()/2,height()/2),
         pos(width()/2,height()/2),
         color(250,250,50,50),
+        area(),
     ])
 }
 
 let shelldonPowerCollide = function shelldonPowerCollide(){
     onCollide('enemy','shelldonpowerup', (e)=>{
-        e.pretzelSpeed = 0;
+        e.speed = e.speed / 2
     })
+}
+
+let larsPowerCollide = function larsPowerCollide(){
+    onCollide('enemy','larspowerup', (e)=>{
+        e.speed = 0
+        powerTimer()
+    })
+}
+
+function powerTimer (){
+    const pTimer = add([
+        text('0'),
+        pos(300, 50),
+        scale(2),
+        layer('obj'),
+        "pTimer",
+        {
+            time: PTIME_COUNT,
+        },
+    ])
+
+    pTimer.onUpdate(() => {
+        pTimer.time += dt()
+        pTimer.text = pTimer.time.toFixed(2)
+
+    })
+
 }
 
 
 
-export{larsPowers, texPowers,shelldonPowers, rainettePowers, rainaPowers, ivyPowers, dwaynePowers, shuihaiziPowers, bartholomewPowers, shelldonPowerCollide};
+
+export{larsPowers, texPowers,shelldonPowers, rainettePowers, rainaPowers, ivyPowers, dwaynePowers, shuihaiziPowers, bartholomewPowers, shelldonPowerCollide, larsPowerCollide};
