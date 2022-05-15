665814a507f58c8904f00df6b12340e3010b378f
Archie Lee <archie.lee@sonomaacademy.org>
2022-05-15 00:04:24 -0700

    Makes the level resets actually work

--- lose.js
+++ lose.js
@@ -37,9 +37,9 @@ scene("lose",() =>{
             if (btn.isHovering()) {
                 const t = time() * 10
                 btn.color = rgb(
-                    wave(0, 255, t),
-                    wave(0, 255, t + 2),
-                    wave(0, 255, t + 4),
+                    wave(255, 255, t),
+                    wave(255, 255, t + 2),
+                    wave(255, 255, t + 4),
                 )
                 btn.scale = vec2(1.2)
             } else {
